a1f09a2a6442eaf1eebcd82f3b4c686cda588985
Hjalte <h.jaltehb123@gmail.com>
2024-07-25 10:06:18 +0200

    Updated BatchTable to match new DTO model from backend

--- src/components/BatchTable.js
+++ src/components/BatchTable.js
@@ -3,13 +3,17 @@ import { Table } from 'react-bootstrap';
 
 const BatchTable = ({ batches }) => {
     return (
-        <Table>
+        <Table class="table table-hover">
             <thead>
                 <tr>
                     <th>ID</th>
                     <th>Start time</th>
                     <th>Sugar</th>
+                    <th>Grams of Sugar</th>
                     <th>Tea</th>
+                    <th>Grams of Tea</th>
+                    <th>Steep minutes</th>
+                    <th>Description</th>
                 </tr>
             </thead>
             <tbody>
@@ -17,8 +21,12 @@ const BatchTable = ({ batches }) => {
                     <tr key={batch.id}>
                         <td>{batch.id}</td>
                         <td>{batch.startTime}</td>
-                        <td>{batch.sugar}</td>
-                        <td>{batch.tea}</td>
+                        <td>{batch.sugarType}</td>
+                        <td>{batch.gramsOfSugar}</td>
+                        <td>{batch.teaType}</td>
+                        <td>{batch.gramsOfTea}</td>
+                        <td>{batch.steepMinutes}</td>
+                        <td>{batch.description}</td>
                     </tr>
                 ))}
             </tbody>
