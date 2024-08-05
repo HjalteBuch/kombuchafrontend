5434a6854948f1d6f82beee0541fc9edc8044dd6
Hjalte <h.jaltehb123@gmail.com>
2024-08-05 08:19:47 +0200

    Added functionality to delete batches.

--- src/pages/batches.js
+++ src/pages/batches.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { getBatches } from '../services/api';
 import BatchTable from '../components/BatchTable';
 import BatchCreation from '../components/BatchCreation';
 
