14bc3c5f3233fbbe2fdd648354aaa06471ca53d7
Hjalte <h.jaltehb123@gmail.com>
2024-09-03 18:20:51 +0200

    added selected ingredients section, only visual for now

--- src/components/BottleCreation.js
+++ src/components/BottleCreation.js
@@ -76,12 +76,25 @@ export const BottleCreation = ({batch, ingredients, getIngredients}) => {
 
                         {/* Ingredients */}
                         <DropdownComponent type={"Ingredients"} setSelectedElement={setSelectedIngredient} setAmountOfSelectedElement={setIngredientAmount} />
-                        <Button className="ms-1 my-4" variant="info">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
-                                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
-                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                            </svg>
-                        </Button>
+                        <div className="col d-flex">
+                            <Button className="ms-1" variant="info">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+                                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                                </svg>
+                            </Button>
+                            <div className="container">
+                                <h3>Selected Ingredients:</h3>
+                                <div className="p-2 d-flex flex-wrap border bg-dark-subtle rounded">
+                                    <div className="d-flex bg-body-secondary rounded-pill">
+                                        <span>Cherry</span>
+                                        <div className="badge-pill badge-dark">
+                                            <Button className="ml-2" variant="close"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
                         {/* Description */}
                         <div className="mb-3">
