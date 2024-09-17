dedef16570a4ef81fb2e772c75081bbd128ba9c3
Hjalte <h.jaltehb123@gmail.com>
2024-09-17 15:56:46 +0200

    Showing bottle on review page

--- src/pages/BottleReviewCreation.js
+++ src/pages/BottleReviewCreation.js
@@ -1,24 +1,59 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 import StarRating from '../components/StarRating';
+import BottleCard from '../components/BottleCard';
+import BottleCardLoader from "../components/BottleCardLoader";
 
 const baseUrl = process.env.REACT_APP_BACKEND_URL;
 
 const BottleReviewCreation = () => {
+    const { bottleName } = useParams();
+    const [bottle, setBottle] = useState();
+    const [isBottle, setIsBottle] = useState(false);
     const [fizzLevel, setFizzLevel] = useState(1);
     const [funkLevel, setFunkLevel] = useState(1);
     const [tasteLevel, setTasteLevel] = useState(1);
     const [overAllRating, setOverAllRating] = useState(1);
+    const [comment, setComment] = useState("");
+
+    const handleTextChange = (e) => {
+        setComment(e.target.value);
+    };
 
     const postReview = async (e) => {
         e.preventDefault();
-        console.log("fizzLevel: " + fizzLevel);
-        console.log("funkLevel: " + funkLevel);
-        console.log("tasteLevel: " + tasteLevel);
-        console.log("overAllRating: " + overAllRating);
+        var review = {
+            BottleId: bottle.Id,
+            FizzLevel: fizzLevel,
+            FunkLevel: funkLevel,
+            TasteLevel: tasteLevel,
+            OverAllRating: overAllRating,
+            Description: comment,
+        }
+        console.log(bottle);
     }
+
+    const getBottle = async () => {
+        const response = await fetch(`${baseUrl}/Bottle/NewestWithBottleName/${bottleName}`);
+        if (response.ok) {
+            setBottle(await response.json());
+            setIsBottle(true);
+        }
+    }
+
+    useEffect(() => {
+        getBottle();
+    }, []);
     return (
         <div className="container-xl align-middle text-center mt-5">
+            <div className="w-50 mx-auto">
+                { isBottle ? (
+                    <BottleCard bottle={bottle} />
+                ) : (
+                        <BottleCardLoader />
+                    )}
+            </div>
             <form>
                 <h1 className="fw-bolder">BottleName</h1>
                 <StarRating metric="Fizz level" rating={fizzLevel} setRating={setFizzLevel} />
@@ -27,7 +62,7 @@ const BottleReviewCreation = () => {
                 <StarRating metric="Overall rating" rating={overAllRating} setRating={setOverAllRating} />
                 <div className="mb-3">
                     <h3>Comments:</h3>
-                    <textarea className="form-control w-50 mx-auto" id="description" rows="3"></textarea>
+                    <textarea className="form-control w-50 mx-auto" id="description" rows="3" value={comment} onChange={handleTextChange}></textarea>
                 </div>
                 <Button variant="primary" onClick={postReview}>Submit review</Button>
             </form>
