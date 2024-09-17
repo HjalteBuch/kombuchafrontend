ea3ee2af9b084151739dd3270d0bbecf868a36a0
Hjalte <h.jaltehb123@gmail.com>
2024-09-17 12:34:09 +0200

    Added StarRating for each required metric

--- src/pages/BottleReviewCreation.js
+++ src/pages/BottleReviewCreation.js
@@ -5,11 +5,18 @@ const baseUrl = process.env.REACT_APP_BACKEND_URL;
 
 const BottleReviewCreation = () => {
     const [fizzLevel, setFizzLevel] = useState(1);
+    const [funkLevel, setFunkLevel] = useState(1);
+    const [tasteLevel, setTasteLevel] = useState(1);
+    const [overAllRating, setOverAllRating] = useState(1);
+
     return (
         <div className="container-xl align-middle text-center mt-5">
             <form>
                 <h1 className="fw-bolder">BottleName</h1>
                 <StarRating metric="Fizz level" rating={fizzLevel} setRating={setFizzLevel} />
+                <StarRating metric="Funk level" rating={funkLevel} setRating={setFunkLevel} />
+                <StarRating metric="Taste level" rating={tasteLevel} setRating={setTasteLevel} />
+                <StarRating metric="Overall Rating" rating={overAllRating} setRating={setOverAllRating} />
             </form>
         </div>
     );
